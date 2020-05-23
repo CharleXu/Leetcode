d89973af663767d7f88bfcff3691685665ee32af
CharleXu <superxc0102@gmail.com>
2020-05-23 15:13:29 -0400

    leetcode_1

---
+++ leetcode_1.py
@@ -0,0 +1,14 @@
+def twoSum(nums, target):
+    num_dict = {}
+    for i in range(len(nums)):
+        res = target - nums[i]
+        if res in num_dict:
+            return [num_dict[res], i]
+        num_dict[nums[i]] = i
+    return []
+
+
+if __name__ == "__main__":
+    nums = [3, 2, 4]
+    target = 6
+    print(twoSum(nums, target))
