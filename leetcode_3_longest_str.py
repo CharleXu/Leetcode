e551be61ce8c68e6b44b362c78866bce4709e5db
CharleXu <superxc0102@gmail.com>
2020-05-25 19:30:36 -0400

    leetcode-3: Longest Substring without repeating characters

---
+++ leetcode_3_longest_str.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+
+def length_of_longest_substring(s):
+    if not s:
+        return 0
+    n = len(s)
+    sub_str = dict()
+    ans = 0
+    i = 0
+    for j in range(n):
+        if s[j] in sub_str.keys():
+            i = max(sub_str[s[j]], i)
+        ans = max(ans, j - i + 1)
+        sub_str[s[j]] = j + 1
+
+    return ans
+
+
+s = 'pwwkew'
+print(length_of_longest_substring(s))
