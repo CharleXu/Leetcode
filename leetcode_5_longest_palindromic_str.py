e3ac7319ff8539d5fc60a6f544c3678ac9aac432
CharleXu <superxc0102@gmail.com>
2020-05-26 19:57:14 -0400

    leetcode-5: longest palindromic substring

---
+++ leetcode_5_longest_palindromic_str.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+
+def longest_palindrome(s):
+    if not s:
+        return ""
+    n = len(s)
+    start, end = 0, 0
+    for i in range(n):
+        len1 = _expand_around_center(s, i, i)
+        len2 = _expand_around_center(s, i, i + 1)
+        len3 = max(len1, len2)
+        if len3 > end - start:
+            start = i - (len3 - 1) // 2
+            end = i + len3 // 2
+    # print(int(start), int(end+1))
+    return s[int(start):int(end + 1)]
+
+
+def _expand_around_center(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    return right - left - 1
+
+
+if __name__ == "__main__":
+    s = 'abcdcbaefg'
+    print(longest_palindrome(s))
