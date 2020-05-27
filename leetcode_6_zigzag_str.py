2bc20f05d4aa457d9143b4d49d77574a054eb557
CharleXu <superxc0102@gmail.com>
2020-05-26 21:31:57 -0400

    leetcode-6: zigzag conversion

---
+++ leetcode_6_zigzag_str.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+
+def convert(s, rows):
+    if rows == 1:
+        return s
+    ret = []
+    n = len(s)
+    cycle = 2 * rows - 2
+    for i in range(rows):
+        for j in range(0, n - i, cycle):
+            ret.append(s[j+i])
+            if i != 0 and i != rows - 1 and j + cycle - i < n:
+                ret.append(s[j + cycle - i])
+    print(ret)
+    return "".join(ret)
+
+
+if __name__ == "__main__":
+    s = "PAYPALISHIRING"
+    num_rows = 4
+    print(convert(s, num_rows))
