a6397ed121aee4a50d24922e9706229553b2076e
CharleXu <superxc0102@gmail.com>
2020-05-26 23:10:23 -0400

    leetcode-7: reverse interger

--- leetcode_7_reverse_interger.py
+++ leetcode_7_reverse_interger.py
@@ -12,6 +12,18 @@ def reverse(x):
     return ret if -2 ** 31 <= ret <= 2 ** 31 - 1 else 0
 
 
+def reverse2(x):
+    sign = -1 if x < 0 else 1
+    x = abs(x)
+
+    rev = 0
+    while x:
+        x, mod = divmod(x, 10)
+        rev = rev * 10 + mod
+
+    return rev*sign if -2 ** 31 <= rev*sign <= 2 ** 31 - 1 else 0
+
+
 if __name__ == "__main__":
     num = 1534236469
     print(reverse(num))
