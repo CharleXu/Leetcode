1ba3111401079769282cadd3fb9c1e3882ce3b90
CharleXu <superxc0102@gmail.com>
2020-06-06 18:15:59 -0400

    leetcode-48: Rotate Image

---
+++ leetcode_48_Rotate_Image.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+
+def rotate(matrix):
+    """
+    Rotate the matrix in place by 90 degrees (clockwise)
+    :param matrix: List[List[int]]
+    :return: None
+    """
+    if not matrix:
+        return
+
+    n = len(matrix)
+    step = n
+    if step % 2 != 0:
+        step += 1
+
+    for i in range(n // 2):
+        for j in range(step // 2):
+            matrix[i][j], matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j], \
+            matrix[n - 1 - j][i] = matrix[n - 1 - j][i], matrix[i][j], \
+                                   matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j]
+
+
+if __name__ == "__main__":
+    image = [
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
+    ]
+    rotate(image)
+    print(image)
