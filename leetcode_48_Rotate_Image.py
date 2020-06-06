f37b67311a1e282826297dee679d0fc8bb9af8ae
CharleXu <superxc0102@gmail.com>
2020-06-06 18:17:39 -0400

    leetcode-48: Rotate Image

--- leetcode_48_Rotate_Image.py
+++ leetcode_48_Rotate_Image.py
@@ -1,25 +1,33 @@
 # coding: utf-8
 
 
+# def rotate(matrix):
+#     """
+#     Rotate the matrix in place by 90 degrees (clockwise)
+#     :param matrix: List[List[int]]
+#     :return: None
+#     """
+#     if not matrix:
+#         return
+#
+#     n = len(matrix)
+#     step = n
+#     if step % 2 != 0:
+#         step += 1
+#
+#     for i in range(n // 2):
+#         for j in range(step // 2):
+#             matrix[i][j], matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j], \
+#             matrix[n - 1 - j][i] = matrix[n - 1 - j][i], matrix[i][j], \
+#                                    matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j]
+
 def rotate(matrix):
     """
     Rotate the matrix in place by 90 degrees (clockwise)
     :param matrix: List[List[int]]
     :return: None
     """
-    if not matrix:
-        return
-
-    n = len(matrix)
-    step = n
-    if step % 2 != 0:
-        step += 1
-
-    for i in range(n // 2):
-        for j in range(step // 2):
-            matrix[i][j], matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j], \
-            matrix[n - 1 - j][i] = matrix[n - 1 - j][i], matrix[i][j], \
-                                   matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j]
+    matrix[:] = zip(*matrix[::-1])
 
 
 if __name__ == "__main__":
