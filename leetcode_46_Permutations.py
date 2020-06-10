bd844744be4bf6f026782fe69909bd1cb6318eb4
CharleXu <superxc0102@gmail.com>
2020-06-10 16:11:34 -0400

    leetcode-46: Permutations

--- leetcode_46_Permutations.py
+++ leetcode_46_Permutations.py
@@ -1,31 +1,52 @@
 # coding: utf-8
 
 
+# def permute(nums):
+#     """
+#
+#     :param nums: List[int]
+#     :return: List[List[int]]
+#     """
+#
+#     def _helper(n):
+#         if len(n) == 1:
+#             return [[n[0]]]
+#
+#         ret = []
+#         for j in range(len(n)):
+#             n_copy = n.copy()
+#             n_copy.remove(n[j])
+#             res = _helper(n_copy)
+#             perm = [[n[j]] + i for i in res]
+#             ret.extend(perm)
+#
+#         return ret
+#
+#     return _helper(nums)
+
+
 def permute(nums):
     """
 
     :param nums: List[int]
     :return: List[List[int]]
     """
+    ret = []
 
-    def _helper(n):
-        if len(n) == 1:
-            return [[n[0]]]
+    def _helper(nums, p, q):
+        if p == q:
+            ret.append(nums)
+        else:
+            for i in range(p, q+1):
+                nums[i], nums[p] = nums[p], nums[i]
+                _helper(nums, p+1, q)
+                nums[i], nums[p] = nums[p], nums[i]
 
-        ret = []
-        for j in range(len(n)):
-            n_copy = n.copy()
-            n_copy.remove(n[j])
-            res = _helper(n_copy)
-            perm = [[n[j]] + i for i in res]
-            ret.extend(perm)
-
-        return ret
-
-    return _helper(nums)
+    _helper(nums, 0, len(nums)-1)
+    return ret
 
 
 if __name__ == "__main__":
-    li = [1, 2, 3]
+    li = [1, 2, 3, 4]
     print(permute(li))
 
